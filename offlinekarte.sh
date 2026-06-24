71dff3221f3b399c8af912280a9e93c50c3c060c
Patko <23560741+Patochan@users.noreply.github.com>
2026-06-24 09:51:23 +0200

    extract offlinekarte from generic setup

---
+++ offlinekarte.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -e
+
+CMD=$1
+APPNAME=offlinekarte
+
+if [ "$CMD" = "init" ]; then
+    echo "Running INIT workflow for ${APPNAME}..."
+	mkdir -p $OFFLINEKARTE_PATH
+	cd $OFFLINEKARTE_PATH
+	git clone $OFFLINEKARTE_GIT -b $OFFLINEKARTE_BRANCH $OFFLINEKARTE_PATH
+	git submodule update --init --recursive
+
+	bash $OFFLINEKARTE_PATH/zskarte.sh init
+
+	echo "Running init for zskarte submodule"
+	
+	if [ ! -f "$ZSKARTE_PATH/packages/server/.env" ]; then
+		cp "$ZSKARTE_PATH/packages/server/.env.example" "$ZSKARTE_PATH/packages/server/.env"
+	fi
+		
+	mkdir -p $ZSKARTE_PATH/data/postgresql
+	sudo chown -R 1001:1001 $ZSKARTE_PATH/data/postgresql
+	mkdir -p $ZSKARTE_PATH/data/pgadmin
+	sudo chown -R 5050:5050 $ZSKARTE_PATH/data/pgadmin
+
+	if [ -f "$ZSKARTE_ENV_TS" ]; then
+		echo "Passe TypeScript Environments an..."
+		
+		# Protokoll dynamisch anhand der TLS-Variable bestimmen
+		if [ "$ZSKARTE_ENABLE_TLS" = "true" ]; then
+			PROTO="https"
+			echo "TLS ist aktiviert -> Nutze https://"
+		else
+			PROTO="http"
+			echo "TLS ist deaktiviert -> Nutze http://"
+		fi
+
+		sed -i "s|apiUrl:.*|apiUrl: '${PROTO}://${ZSKARTE_API_DOMAIN}${ZSKARTE_API_PATH}',|" "$ZSKARTE_ENV_TS"
+		sed -i "s|tileUrl:.*|tileUrl: '${PROTO}://${OFFLINEKARTE_TILE_DOMAIN}${OFFLINEKARTE_TILE_PATH}',|" "$ZSKARTE_ENV_TS"
+		sed -i "s|searchUrl:.*|searchUrl: '${PROTO}://${OFFLINEKARTE_SEARCH_DOMAIN}${OFFLINEKARTE_SEARCH_PATH}',|" "$ZSKARTE_ENV_TS"
+		sed -i "s|searchLabel:.*|searchLabel: '${ZSKARTE_SEARCH_LABEL}',|" "$ZSKARTE_ENV_TS"
+		
+		echo "TypeScript Environments erfolgreich aktualisiert."
+		echo "----------------------------------------"
+		grep -E "apiUrl|tileUrl|searchUrl|searchLabel" "$ZSKARTE_ENV_TS"
+		echo "----------------------------------------"
+	else
+		echo "Warnung: $ZSKARTE_ENV_TS nicht gefunden. ueberspringe Anpassung."
+	fi
+	echo "----------------------------------------"
+	echo "offlinekarte Konfiguration (inkl. override)"
+	echo "----------------------------------------"
+	docker compose -f $OFFLINEKARTE_PATH/docker-compose.yml -f docker-compose-offlinekarte.yml config
+	echo "----------------------------------------"
+	echo "zskarte Konfiguration (inkl. override)"
+	echo "----------------------------------------"
+	docker compose -f $ZSKARTE_PATH/docker-compose.yml -f docker-compose-zskarte.yml config
+	echo "----------------------------------------"
+	
+    echo "${APPNAME} complete."
+
+elif [ "$CMD" = "start" ]; then
+    echo "Starting ${APPNAME}..."
+
+    docker compose up -d -f $OFFLINEKARTE_PATH/docker-compose.yml -f docker-compose-offlinekarte.yml
+	docker compose up -d -f $ZSKARTE_PATH/docker-compose.yml -f docker-compose-zskarte.yml
+
+    echo "${APPNAME} started."
+
+elif [ "$CMD" = "start" ]; then
+    echo "Updating ${APPNAME}..."
+
+    # to be build
+
+    echo "${APPNAME} updated."
+else
+    echo "Usage: ./offlinekarte.sh {init|update|start}"
+    exit 1
+fi
