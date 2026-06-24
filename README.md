64f70e4255936c7f68b2aeb6f242d22e6845c5a4
Patko <23560741+Patochan@users.noreply.github.com>
2026-06-24 15:04:17 +0200

    adding pwa script

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # zso_apps_proxy
-Setup Scripte für die Einrichtung des ZSO Apps Proxy (Traefik) und der Grundeinrichtung von ZSO Apps.
+Setup Scripte für die Einrichtung des ZSO Apps Proxy (Traefik) und der Grundeinrichtung von ZSO Apps (ZSO App, Incident-Manager und Offlinekarte)
+
+Starte mit:
+git clone https://github.com/ZSO-Apps/zso_apps_proxy.git
+
+cd zso_apps_proxy
+cp .env.example .env
+
+
+RedHat Based (dont use Podman!)
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin openssl
+
+Debian based:
+apt-get install -y git docker.io docker-compose-v2 openssl
+
+Docker-Services müssen aktiviert sein:
+	systemctl start docker
+	systemctl enable docker
+
+
+
+SELinux auf RHEL based:
+setsebool -P container_manage_cgroup on || true
+
+dnf install -y policycoreutils-python-utils
+semanage fcontext -a -t container_runtime_tmp_t "/run/docker.sock" 2>/dev/null || true
+restorecon -v /run/docker.sock || true
+	
---
+++ zso_pwa_app.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+CMD=$1
+APPNAME=PWA
+SCRIPTDIR=$(pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+
+if [ -f ".env" ]; then
+    echo "Lade Konfiguration aus .env Datei..."
+    set -a
+    source .env
+    set +a
+else
+    echo "Error: Zentrale .env Datei im aktuellen Verzeichnis nicht gefunden!"
+    exit 1
+fi
+
+# Precheck
+if ! docker info > /dev/null 2>&1; then
+    echo "Error: Docker läuft aktuell nicht!" >&2
+    exit 1
+fi
+
+if [ "$CMD" = "init" ]; then
+    echo "Initialize ${APPNAME}..."
+	
+	if [ ! -d "$PWA_APP_PATH" ]; then
+        git clone $PWA_GIT $PWA_APP_PATH
+    fi
+	
+    echo "${APPNAME} initialized."
+
+elif [ "$CMD" = "start" ]; then
+    echo "Starting ${APPNAME}..."
+	docker compose -f $PWA_APP_PATH/docker-compose.yml -f $SCRIPTDIR/docker-compose-pwa.yml --env-file $SCRIPTDIR/.env up -d
+	
+    echo "${APPNAME} started."
+
+elif [ "$CMD" = "stop" ]; then
+    echo "Stopping ${APPNAME}..."
+	docker compose -f $PWA_APP_PATH/docker-compose.yml -f $SCRIPTDIR/docker-compose-pwa.yml --env-file $SCRIPTDIR/.env stop
+
+    echo "${APPNAME} stopped."
+elif [ "$CMD" = "config" ]; then
+    echo "Load config of ${APPNAME}..."
+	
+	echo "----------------------------------------"
+	docker compose -f $PWA_APP_PATH/docker-compose.yml -f $SCRIPTDIR/docker-compose-pwa.yml --env-file $SCRIPTDIR/.env config
+	echo "----------------------------------------"
+else
+    echo "Usage: ./zso_pwa_app.sh {start|stop|config}"
+    exit 1
+fi
