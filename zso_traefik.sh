1490aa313ef0ea4307be5f3469aa254ab11f5c55
Patko <23560741+Patochan@users.noreply.github.com>
2026-06-24 12:50:44 +0200

    entferne docker installation aus zso_traefik script (neu in readme)

--- zso_traefik.sh
+++ zso_traefik.sh
@@ -37,52 +37,16 @@ echo "Pruefe Container-Engine Verfuegbarkeit..."
 	if command -v dnf &> /dev/null; then
 		IS_RHEL=true
 		echo "RHEL based system erkannt!"
-		echo "Versuche Docker ueber DNF zu installieren..."
-		if dnf install -y docker docker-compose-plugin openssl 2>/dev/null; then
-			echo "Docker erfolgreich installiert."
-		else
-			echo "Docker-Repository nicht verfuegbar. Weiche auf Docker Repo aus"
-			sudo dnf -y install dnf-plugins-core
-			sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-			sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin openssl
-		fi
 	elif command -v apt-get &> /dev/null; then
-		IS_RHEL=false
 		echo "Debian based system erkannt!"
-		apt-get update
-		apt-get install -y git docker.io docker-compose-v2 openssl
 	elif [ -f /etc/synoinfo.conf ]; then
 		IS_RHEL=false
-		echo "Synology NAS erkannt. ueberpruefe installierte Pakete..."
-		
-		# Git und OpenSSL koennen via SynoCommunity (opkg) oder oft direkt genutzt werden.
-		# Docker/Docker-Compose sollte ueber das DSM Paketzentrum installiert sein.
-		if ! command -v docker &> /dev/null || ! docker compose version &> /dev/null; then
-			echo "Fehler: Docker oder docker compose ist nicht auf der Synology installiert!"
-			echo "Bitte installiere das 'Container Manager' (DSM 7.2+) oder 'Docker' Paket ueber das DSM Paketzentrum."
-			exit 1
-		fi
-		#fallback fuer synology NAS ohne seccomp
-		echo "Docker und docker compose sind auf der Synology bereit."
+		echo "Synology NAS erkannt. Applikationen vermutlich nicht supportet..."
 	else
-		echo "Fehler: Nicht unterstuetzte Distribution!"
+		echo "Fehler: Vermutlich nicht unterstuetzte Distribution!"
 		exit 1
 	fi
-
-	echo "Konfiguriere Docker-Dienst..."
-	systemctl start docker
-	systemctl enable docker
-
-	# SELinux Berechtigungen (Nur fuer RHEL-Systeme relevant)
-	if [ "$IS_RHEL" = true ]; then
-		echo "Konfiguriere SELinux-Booleans..."
-		setsebool -P container_manage_cgroup on || true
-		
-		dnf install -y policycoreutils-python-utils
-		semanage fcontext -a -t container_runtime_tmp_t "/run/docker.sock" 2>/dev/null || true
-		restorecon -v /run/docker.sock || true
-	fi
-
+	
 	# Let's Encrypt Ordner und Rechte vorbereiten
 	mkdir -p ./certs
 	touch ./certs/acme.json
